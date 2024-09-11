dd88df6043917567f690a124e974f1348ae737b8
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2024-09-11 19:27:25 -0300

    style: adding style guide to tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,33 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import {
+  Nunito_Sans,
+  Darker_Grotesque,
+  Red_Hat_Display,
+} from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Gerador de cartão de visita",
   description: "Gerador de cartão de visita da Resultados Digitais",
 };
+
+const nunito = Nunito_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-nunito",
+});
+
+const darkerGrotesque = Darker_Grotesque({
+  subsets: ["latin"],
+  weight: ["400", "700", "800", "900"],
+  variable: "--font-darker-grotesque",
+});
+
+const redHatDisplay = Red_Hat_Display({
+  subsets: ["latin"],
+  weight: ["700"],
+  variable: "--font-red-hat-display",
+});
 
 export default function RootLayout({
   children,
@@ -26,7 +37,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={
+          nunito.variable +
+          " " +
+          darkerGrotesque.variable +
+          " " +
+          redHatDisplay.variable
+        }
       >
         {children}
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,36 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      headingGrotesque: "var(--font-darker-grotesque)",
+      headingRedHat: "var(--font-red-hat-display)",
+      primary: "var(--font-nunito)",
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        purple: {
+          400: "#6C5AE0",
+          500: "#4839A3",
+          700: "#100C27",
+        },
+        yellow: {
+          400: "#F4CA6D",
+          500: "#F2BF4E",
+        },
+        black: "#212429",
+        white: "#FFFFFF",
+        gray: {
+          200: "#F1F3F5",
+          300: "#CFD3D8",
+          400: "#97A1AC",
+          500: "#7E8A98",
+        },
       },
     },
   },
